983f8af0d5d6c00a12f1b2b5559806a65ed5c8ed
Nguyen Hoang Son <nguyenhoangson@cityascom.vn>
2023-04-27 14:35:16 +0700

    step 4

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
 import 'firebase_options.dart';
 
 void main() {
@@ -19,6 +18,7 @@ void main() {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/notes/': (context) => const NotesView(),
       },
     ),
   );
@@ -95,7 +95,6 @@ class _NotesViewState extends State<NotesView> {
                     Navigator.of(context)
                         .pushNamedAndRemoveUntil('/login/', (route) => false);
                   }
-                  devtools.log(shouldLogout.toString());
                   break;
               }
             },
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -57,17 +58,16 @@ class _RegisterViewState extends State<RegisterView> {
                       final userCredential = await FirebaseAuth.instance
                           .createUserWithEmailAndPassword(
                               email: email, password: password);
-                      print(userCredential);
+                      devtools.log(userCredential.toString());
                     } on FirebaseAuthException catch (e) {
                       if (e.code == 'weak-password') {
-                        print("weak password");
+                        devtools.log("weak password");
                       } else if (e.code == "email-already-in-use") {
-                        print("email already in use");
+                        devtools.log("email already in use");
                       } else {
-                        print(e.code);
+                        devtools.log(e.code);
                       }
-
-                      print(e.code);
+                      devtools.log(e.code);
                     }
                   },
                   child: const Text("Register"))),
